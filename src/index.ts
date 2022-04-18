df324103aeb9221a8c29d48745af979971059b18
aibayanyu <aibayanyu@qq.com>
2022-04-18 21:37:49 +0800

    chore: add playground

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { Plugin } from 'vite'
 import type { StarterOptions } from './typing'
 
-export const starterPlugin = (opt: StarterOptions): Plugin => {
+export const starterPlugin = (_opt: StarterOptions): Plugin => {
   return {
     name: 'vite-plugin-starter',
   }
